86846510ead491c7e78a2dc9482be9c0d2602977
一半月q <1953408947@qq.com>
2025-04-01 14:40:25 +0800

    Add files via upload

---
+++ springboot_wms/wms/src/sql/table.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(20) DEFAULT NULL COMMENT '账号',
+  `name` varchar(100) NOT NULL COMMENT '名字',
+  `password` varchar(20) NOT NULL COMMENT '密码',
+  `age` int(11) DEFAULT NULL,
+  `sex` int(11) DEFAULT NULL COMMENT '性别',
+  `phone` varchar(20) DEFAULT NULL COMMENT '电话',
+  `role_id` int(11) DEFAULT NULL COMMENT '角色 0超级管理员，1管理员，2普通账号',
+  `isValid` varchar(4) DEFAULT 'Y' COMMENT '是否有效，Y有效，其他无效',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+BEGIN;
+INSERT INTO `user` VALUES (1, 'sa', '超级管理员', '123', 18, 1, '111', 0, 'Y');
+COMMIT;
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `menuCode` varchar(8) DEFAULT NULL COMMENT '菜单编码',
+  `menuName` varchar(16) DEFAULT NULL COMMENT '菜单名字',
+  `menuLevel` varchar(2) DEFAULT NULL COMMENT '菜单级别',
+  `menuParentCode` varchar(8) DEFAULT NULL COMMENT '菜单的父code',
+  `menuClick` varchar(16) DEFAULT NULL COMMENT '点击触发的函数',
+  `menuRight` varchar(8) DEFAULT NULL COMMENT '权限 0超级管理员，1表示管理员，2表示普通用户，可以用逗号组合使用',
+  `menuComponent` varchar(200) DEFAULT NULL,
+  `menuIcon` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+BEGIN;
+INSERT INTO `menu` VALUES (1, '001', '管理员管理', '1', NULL, 'Admin', '0', 'admin/AdminManage.vue', 'el-icon-s-custom');
+INSERT INTO `menu` VALUES (2, '002', '用户管理', '1', NULL, 'User', '0,1', 'user/UserManage.vue', 'el-icon-user-solid');
+COMMIT;
